650e8958b8db6a68b5b352878609c8c594b6fb8b
Sarkout Mahmoud <s.mahmoud-1@student.tudelft.nl>
2016-05-01 21:37:42 +0200

    Some more progress, At the moment I get a "This cannot be run by Goal; please selct a valid GOAL file" error.

--- UTbot.pl
+++ UTbot.pl
@@ -1,4 +1,3 @@
 :-dynamic navigation/2, navPoint/3, self/3, orientation/3, status/2, score/3, currentWeapon/2, weapon/3,
-	powerup/2, armor/4, fragged/4, path/4, logic/1, pickup/3, base/2, game/4, teamScore/2, flagState/2, item/4,
-	bot/6, pickup/1, slowVolume/1, keepMoving/0, getHp/0, getItem/1, setWeapPref/0, kill/2, shoot/1,pickedUp/1,
-	prevLocation/3, look/1, goTo/1, resetLook/0.
+	 armor/4, fragged/4,  pickup/3, base/2,
+	    powerup/2 ,prevLocation/3.
