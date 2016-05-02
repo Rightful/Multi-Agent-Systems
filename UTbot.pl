2b31d981320986b03a17b9c2e9454a4dc5ddb3b9
Sarkout Mahmoud <s.mahmoud-1@student.tudelft.nl>
2016-05-02 20:34:36 +0200

    some progress on the CaptureTheFlag module. Also fixed a small bug

--- UTbot.pl
+++ UTbot.pl
@@ -1,3 +1,3 @@
 :-dynamic navigation/2, navPoint/3, self/3, orientation/3, status/2, score/3, currentWeapon/2, weapon/3,
-	 armor/4, fragged/4,  pickup/3, base/2,
+	 armor/4, fragged/4,  pickup/3, base/2,captureFlag/0,flagState/2, flag/3,
 	    powerup/2 ,prevLocation/3.
