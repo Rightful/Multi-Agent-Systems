b3f85296abd04e5e5e54838ddb67d008e15908e5
lvelzel <leroyvelzel@hotmail.com>
2016-05-03 12:13:04 +0200

    Abstract Initmodule
    abstractInit heeft alle percept + deployable, ook deployable toegevoegd aan UTbot.pl

--- UTbot.pl
+++ UTbot.pl
@@ -1,3 +1,3 @@
 :-dynamic navigation/2, navPoint/3, prevLocation/3, self/3, orientation/3, status/2, score/3, currentWeapon/2, weapon/3,
 	powerup/2, armor/4, fragged/4, path/4, logic/1, pickup/3, base/2, game/4, teamScore/2, flagState/2, item/4,
-	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3.
+	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3, deployable/1.
