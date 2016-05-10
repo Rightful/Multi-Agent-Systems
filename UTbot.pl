fa8f13b45d7f94ebde34946bf04209fa4787e89d
Faysal <f.elbouaiadi@student.tudelft.nl>
2016-05-10 09:02:59 +0200

    Fixed minor bugs.

--- UTbot.pl
+++ UTbot.pl
@@ -1,6 +1,6 @@
 :-dynamic navigation/2, navPoint/3, prevLocation/3, self/3, orientation/3, status/2, score/3, currentWeapon/2, weapon/3,
 	powerup/2, armor/4, fragged/4, path/4, logic/1, pickup/3, base/2, game/4, teamScore/2, flagState/2, item/4,
-	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3, look/1, shoot/1, enemyBaseLocation/1.
+	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3, look/1, shoot/1, enemyBaseLocation/1, ourBaseLocation/1.
 	
 	% We are at a certain location if the IDs match, or ...
 	at(UnrealID) :- navigation(reached,UnrealID).
