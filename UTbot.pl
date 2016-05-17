475e0966d1a57dc6e0ae586842180ab5fd95687b
Sebastiaan Hester <sebastiaanhester@gmail.com>
2016-05-17 10:39:56 +0200

    fixes accidental merge conflict

--- UTbot.pl
+++ UTbot.pl
@@ -1,10 +1,16 @@
 :-dynamic navigation/2, navPoint/3, prevLocation/3, self/3, orientation/3, status/2, score/3, currentWeapon/2, weapon/3,
 	powerup/2, armor/4, fragged/4, path/4, logic/1, pickup/3, base/2, game/4, teamScore/2, flagState/2, item/4,
 	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3, look/1, shoot/1, enemyBaseLocation/1, 
-	ourBaseLocation/1, lastKnownLocFlag/1, base/2, bot/6, deployable/1, goLocation/1, inRadius/1.
-	
+	ourBaseLocation/1, lastKnownLocFlag/1, base/2, bot/6, deployable/1, goLocation/1, inRadius/1, lastAngleUpdater/1.
+
 radius(1500).
 
+	% Getting the next time for updating look action.
+	timeForUpdate :- 
+		lastAngleUpdater(LastAngleUpdateTime), 
+		get_time(CurrentTime),
+		NextTime is LastAngleUpdateTime+0.3,
+		CurrentTime > NextTime.
 	
 	% We are at a certain location if the IDs match, or ...
 	at(UnrealID) :- navigation(reached,UnrealID).
