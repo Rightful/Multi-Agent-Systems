3508b8595faf95adb06047bc9d1b71f3451f04c0
lvelzel <leroyvelzel@hotmail.com>
2016-05-20 17:30:42 +0200

    Escort error

--- UTbot.pl
+++ UTbot.pl
@@ -3,7 +3,8 @@
 	bot/6, pickup/1, slowVolume/1, getItem/1 ,captureFlag/0,flagState/2, flag/3, look/1, shoot/1, enemyBaseLocation/1, 
 	ourBaseLocation/1, lastKnownLocFlag/1, base/2, bot/6, deployable/1, goLocation/1, inRadius/1, pickup/0, escort/0, killModus/0, 
 	angle/3, viewAngle/1, lastAngleUpdater/1, ownLocation/3, lookingAt/1, lastFlag/3, goLocationFlag/1, inRadius/1, 
-	helpDef/2, goLocationHelp/1, goLocationHome/1, goLocationFlagEnemy/1, weaponSelected/0, fraggedBot/2, fraggedBot/1.
+	helpDef/2, goLocationHelp/1, goLocationHome/1, goLocationFlagEnemy/1, weaponSelected/0, fraggedBot/2, fraggedBot/1, goEscort/1, 
+	goHomeWithFlag/1, goLastKnown/1, locationOfFlag/3.
 
 	
 radius(1000).
