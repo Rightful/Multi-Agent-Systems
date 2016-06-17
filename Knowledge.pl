bdbbd33a8b9611ab11bc79f3b7adf60e4685ebc2
Sarkout Mahmoud <s.mahmoud-1@student.tudelft.nl>
2016-06-17 14:09:28 +0200

    wrote somme comments

--- Knowledge.pl
+++ Knowledge.pl
@@ -1,14 +1,45 @@
 :-dynamic 
-	navigation/2, navPoint/3, self/3, orientation/3, lookingAt/1,
-	status/2, score/3, currentWeapon/2, weapon/3, atkRad/1, 
-	powerup/2, armor/4, fragged/4, pickup/3, base/2, flagState/2, 
-	item/4, bot/6, pickup/1, flag/3, enemyBaseLocation/1, 
-	ourBaseLocation/1, base/2, bot/6, goLocation/1, inRadius/1,
-	angle/3, viewAngle/1, lastAngleUpdater/1, ownLocation/3, 
-	lookingAt/1, goLocationFlag/1, goLocationHelp/2, 
-	goLocationFlagEnemy/1, goHomeWithFlag/1, goRandom/1,
-	goLocationHelp/1, goLocationKill/1, goToEnemyBase/1, 
-	closeToEnemyBaseNavPoint/1, closeToOurBaseNavPoint/1.
+	navigation/2, 		%
+	navPoint/3,
+	self/3,
+	orientation/3,
+	lookingAt/1,
+	status/2,
+	%score/3,
+	currentWeapon/2,
+	weapon/3,
+	atkRad/1, 
+	powerup/2,
+	armor/4,
+	fragged/4,
+	pickup/3,
+	base/2,
+	flagState/2, 
+	item/4,
+	bot/6,
+	pickup/1,
+	flag/3,
+	enemyBaseLocation/1, 
+	ourBaseLocation/1,
+	base/2,
+	bot/6,
+	goLocation/1,
+	inRadius/1,		%inRadius(
+	angle/3,		%angle(Angle, Sinus, CoSinus)
+	viewAngle/1,
+	lastAngleUpdater/1,
+	ownLocation/3, 
+	lookingAt/1,
+	goLocationFlag/1,
+%	goLocationHelp/2, 
+	goLocationFlagEnemy/1,
+	goHomeWithFlag/1,
+	goRandom/1,
+	goLocationHelp/1,
+	goLocationKill/1,
+	%goToEnemyBase/1, 
+	closeToEnemyBaseNavPoint/1,
+	closeToOurBaseNavPoint/1.
 
 	% Radius of defender which to navigate.
 	radius(1000).
@@ -44,7 +75,7 @@
 	round(X) =:= round(X1), round(Y) =:= round(Y1), round(Z) =:= round(Z1). 
 	
 	% Fragged predicate to adopt suitable goals.
-    	fraggedBot(ID, location(X,Y,Z)):- fragged(_, _, ID, _).
+    	%fraggedBot(ID, location(X,Y,Z)):- fragged(_, _, ID, _).
 
 	% Enemy bot
 	enemyBot(UnrealID, Location) :-
